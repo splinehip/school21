63780f142aa18d54f3a881e39a92d699e5cfb269
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2021-04-20 11:40:05 +0300

    Adjust test14_strncmp

--- tests/libft/srcs/test14_strncmp.c
+++ tests/libft/srcs/test14_strncmp.c
@@ -14,11 +14,11 @@ void	test14_strncmp(void)
 	printf("Test 14.2, strncmp, res: %d\n", strncmp(str_1, str_3, 6));
 
 	printf("\nTest 14, ft_strncmp, str_1: %s, str_2: %s\n", str_1, str_2);
-	printf("Test 14, ft_strncmp, res: %d\n", strncmp(str_1, str_2, 12));
+	printf("Test 14, ft_strncmp, res: %d\n", ft_strncmp(str_1, str_2, 12));
 	printf("\nTest 14.1, ft_strncmp, str_1: %s, str_2: %s, n: 6\n",
 																str_1, str_2);
-	printf("Test 14.1, ft_strncmp, res: %d\n", strncmp(str_1, str_2, 6));
+	printf("Test 14.1, ft_strncmp, res: %d\n", ft_strncmp(str_1, str_2, 6));
 	printf("\nTest 14.2, ft_strncmp, str_1: %s, str_2: %s, n: 6\n",
 																str_1, str_3);
-	printf("Test 14.2, ft_strncmp, res: %d\n", strncmp(str_1, str_3, 6));
+	printf("Test 14.2, ft_strncmp, res: %d\n", ft_strncmp(str_1, str_3, 6));
 }
