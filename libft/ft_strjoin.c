f6f1e27f37d3ec49bd6920c879a8861864cd594d
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2021-04-26 20:34:18 +0300

    Adjust ft_strjoin, ft_substr.

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 10:04:17 by cflorind          #+#    #+#             */
-/*   Updated: 2021/04/26 16:43:50 by cflorind         ###   ########.fr       */
+/*   Updated: 2021/04/26 20:30:53 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,20 @@ char	*ft_strjoin(const char *s1, char const *s2)
 	char		*res;
 	size_t		len;
 
-	if (!s1 || !s2)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	if (s1 == NULL)
+		return ((char *)s2);
+	if (s2 == NULL)
+		return ((char *)s1);
 	if (*s1 == '\0' && *s2 == '\0')
 		return ((char *)ft_calloc(1, sizeof(char)));
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	res = (char *)ft_calloc(len, sizeof(char));
 	if (res == NULL)
 		return (NULL);
-	ft_strlcpy(res, s1, len);
-	ft_strlcpy(res + ft_strlen(s1), s2, len);
-	if (ft_strncmp(res, s1, ft_strlen(s1)) || ft_strncmp(
-			res + ft_strlen(s1), s2, ft_strlen(s2)))
-		return (NULL);
-	return (res);
+	if (ft_strlcpy(res, s1, len) && ft_strlcpy(res + ft_strlen(s1), s2, len))
+		return (res);
+	free(res);
+	return (NULL);
 }
--- tests/libft/srcs/test24_substr..c
+++ tests/libft/srcs/test24_substr..c
@@ -6,7 +6,7 @@ void	test24_substr(void)
 	char	*subs;
 
 	printf("\nTest 24, ft_substr, str: %s, start: 7, n: 0\n", str);
-	subs = ft_substr(str, 7, 0);
+	subs = ft_substr(str, 7, 100000000000);
 	printf("Test 24, ft_substr, res: %s\n", subs);
 	free(subs);
 
