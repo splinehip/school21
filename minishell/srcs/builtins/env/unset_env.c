718d529cf7b62aeef1ab30856cb2669c58383ea0
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2021-12-06 23:27:48 +0300

    Done unset_env

--- minishell/srcs/builtins/env/unset_env.c
+++ minishell/srcs/builtins/env/unset_env.c
@@ -1,22 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.h                                         :+:      :+:    :+:   */
+/*   unset_env.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/06 14:21:50 by cflorind          #+#    #+#             */
-/*   Updated: 2021/12/06 23:23:55 by cflorind         ###   ########.fr       */
+/*   Created: 2021/12/06 23:02:46 by cflorind          #+#    #+#             */
+/*   Updated: 2021/12/06 23:25:37 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTINS_H
-# define BUILTINS_H
+#include "libft.h"
+#include "bool.h"
+#include "builtins.h"
 
-int		do_pwd(char **env);
-void	set_env(char *name, char *value, char ***env);
-void	unset_env(char *name, char **env);
-char	*get_env(char *name, char **env);
-char	*get_env_value(char *name, char **env);
+void	unset_env(char *name, char **env)
+{
+	int		i;
+	char	*tmp;
 
-#endif
+	name = ft_strjoinchr(name, '=');
+	if (name == NULL)
+		return ;
+	i = 0;
+	while (env[i] && ft_strncmp(env[i], name, ft_strlen(name)) != 0)
+		i++;
+	if (env[i] == NULL)
+		return ;
+	tmp = env[i];
+	while (env[i])
+	{
+		env[i] = env[i + 1];
+		i++;
+	}
+	free(tmp);
+}
