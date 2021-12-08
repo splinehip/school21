1f69dbcb0e3eef95af1f5a474303e4201311ec7b
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2021-12-08 17:34:09 +0300

    Escaped sequens handled

--- minishell/srcs/input_handler/parse_cmd.c
+++ minishell/srcs/input_handler/parse_cmd.c
@@ -6,7 +6,7 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 17:33:02 by cflorind          #+#    #+#             */
-/*   Updated: 2021/12/08 17:13:18 by cflorind         ###   ########.fr       */
+/*   Updated: 2021/12/08 17:31:47 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,23 @@ static inline int	is_insertable(char c, char *cmd, t_iter *iter)
 	else if (iter->in_qoutes == false && c != dollar && c != escape
 		&& (c != space || cmd[iter->i - 1] != space))
 		return (true);
+	else if (c == dollar && cmd[iter->i - 1] == escape)
+		return (true);
 	return (false);
 }
 
-static inline int	is_expandable(char c, t_iter *iter)
+static inline int	is_expandable(char c, char *cmd, t_iter *iter)
 {
 	if (iter->in_qoutes == single_quote)
 		return (false);
 	else if (c == dollar)
-		return (true);
+	{
+		if (iter->i == 0)
+			return (true);
+		if (cmd[iter->i - 2] != escape)
+			return (true);
+		return (false);
+	}
 	return (false);
 }
 
@@ -56,7 +64,7 @@ static inline char	*do_parse(char *cmd, char **env)
 			iter.buf[iter.j++] = cmd[iter.i++];
 			iter.buf[iter.j] = 0;
 		}
-		else if (is_expandable(cmd[iter.i++], &iter))
+		else if (is_expandable(cmd[iter.i++], cmd, &iter))
 		{
 			if (do_expand(cmd, &iter, env) == unsucsses)
 				break ;
