9e67dc8d502f20c63f69f0f69f2f66c582ce3a92
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2021-12-16 17:11:31 +0300

    minishell: fix * template extractor

--- minishell/srcs/input_handler/parser/parse_cmd_template.c
+++ minishell/srcs/input_handler/parser/parse_cmd_template.c
@@ -6,7 +6,7 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 15:36:13 by cflorind          #+#    #+#             */
-/*   Updated: 2021/12/16 15:08:57 by cflorind         ###   ########.fr       */
+/*   Updated: 2021/12/16 17:09:52 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,67 +17,66 @@
 #include "input_handler.h"
 
 static inline int	do_check_quotes_or_break(
-	char *cmd, t_extract *args, int *i, char **quote_type)
+	char *cmd, int *i, char **quote_type)
 {
-	if ((cmd[*i] == quote || cmd[*i] == single_quote)
-		&& escaped(cmd, *i) == false)
+	if (cmd[*i] == quote || cmd[*i] == single_quote)
 	{
-		if (*quote_type && cmd[*i] == **quote_type)
-			*quote_type = NULL;
+		if (*quote_type)
+		{
+			if (**quote_type == single_quote && cmd[*i] == **quote_type)
+				*quote_type = NULL;
+			else if (cmd[*i] == **quote_type && escaped(cmd, *i) == false)
+				*quote_type = NULL;
+		}
 		else if (*quote_type == NULL)
 			*quote_type = cmd + *i;
 		return (false);
 	}
-	if ((cmd[*i] == space && escaped(cmd, *i) == false
-			&& *quote_type == NULL) || *i == args->prev_end)
-	{
-		if (*i == args->prev_end && cmd[*i] == space)
-			*i += 1;
+	if (cmd[*i] == space && escaped(cmd, *i) == false && *quote_type == NULL)
 		return (true);
-	}
 	return (false);
 }
 
 static inline void	find_start(char *cmd, t_extract *args)
 {
-	char	opened_quote;
+	char	open_quote;
 	char	*quote_type;
 
 	quote_type = NULL;
 	args->start = args->pchar - cmd;
-	opened_quote = has_opened_quots(cmd, args->prev_end, args->start);
-	if (opened_quote)
+	open_quote = has_opened_quots(cmd, args->prev_end, args->start);
+	if (open_quote)
 	{
 		while (args->start >= args->prev_end)
 		{
-			if (cmd[args->start] == opened_quote
+			if (cmd[args->start] == open_quote
 				&& escaped(cmd, args->start) == false)
 				break ;
 			args->start--;
 		}
 		return ;
 	}
-	while (args->start >= args->prev_end)
+	while (args->start > args->prev_end)
 	{
-		if (do_check_quotes_or_break(cmd, args, &args->start, &quote_type))
-			break ;
 		args->start--;
+		if (do_check_quotes_or_break(cmd, &args->start, &quote_type))
+			break ;
 	}
 }
 
 static inline void	find_end(char *cmd, t_extract *args)
 {
-	char	opened_quote;
+	char	open_quote;
 	char	*quote_type;
 
 	quote_type = NULL;
 	args->end = args->pchar - cmd;
-	opened_quote = has_opened_quots(cmd, args->end, ft_strlen(cmd));
-	if (opened_quote)
+	open_quote = has_opened_quots(cmd, args->end, ft_strlen(cmd));
+	if (open_quote)
 	{
 		while (cmd[args->end] != ends)
 		{
-			if (cmd[args->end++] == opened_quote
+			if (cmd[args->end++] == open_quote
 				&& escaped(cmd, args->end) == false)
 				break ;
 		}
@@ -85,7 +84,7 @@ static inline void	find_end(char *cmd, t_extract *args)
 	}
 	while (cmd[args->end] != ends)
 	{
-		if (do_check_quotes_or_break(cmd, args, &args->end, &quote_type))
+		if (do_check_quotes_or_break(cmd, &args->end, &quote_type))
 			break ;
 		args->end++;
 	}
