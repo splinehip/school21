57c67e7a55a2335fcffaf47ba80f0cb62100bd1f
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2021-12-18 03:49:31 +0300

    minishel: refactoring do_aterisk

--- minishell/srcs/input_handler/parser/parse_cmd_do_asterisk.c
+++ minishell/srcs/input_handler/parser/parse_cmd_do_asterisk.c
@@ -6,7 +6,7 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 18:21:12 by cflorind          #+#    #+#             */
-/*   Updated: 2021/12/17 21:11:46 by cflorind         ###   ########.fr       */
+/*   Updated: 2021/12/18 03:48:32 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,52 +18,46 @@
 
 static inline char	*do_expand_template(char *template_str)
 {
+	if (template_str == NULL)
+		return (NULL);
 	return (ft_strdup(template_str));
 }
 
 static inline int	do_update_res(
-	t_extract *args, char *parsed_str, char **template_str)
+	t_extract *args, char *parsed_str, char *template_str)
 {
-	int		ret;
-	char	*tmp;
-
-	ret = unsucsses;
-	tmp = *template_str;
-	*template_str = do_expand_template(*template_str);
-	free(tmp);
 	if (args->j != 0 || (args->j == 0 && args->res != NULL))
 		if (do_update_buf(" ", (void *)args, pextract, false) == unsucsses)
-			return (ret);
+			return (unsucsses);
 	if (do_update_buf(parsed_str, (void *)args, pextract, false) == sucsses
 		&& do_update_buf(" ", (void *)args, pextract, false) == sucsses)
-		ret = do_update_buf(*template_str, (void *)args, pextract, false);
-	else
-		ret = unsucsses;
-	return (ret);
+		return (do_update_buf(template_str, (void *)args, pextract, false));
+	return (sucsses);
 }
 
 static inline int	do_extract(t_extract *args, char *cmd, char **env)
 {
-	int		res;
+	int		ret;
 	char	*tmp;
 	char	*template_str;
 	char	*parsed_str;
 
-	res = sucsses;
+	ret = unsucsses;
 	ft_printf("EEE: prev_end: %i, start: %i, end: %i\n",
 		args->prev_end, args->start, args->end);
 	tmp = ft_substr(cmd, args->prev_end, args->start - args->prev_end);
 	parsed_str = do_parse(tmp, env);
 	free(tmp);
 	template_str = ft_substr(cmd, args->start, args->end - args->start);
-	if (parsed_str == NULL || template_str == NULL)
-		res = unsucsses;
+	tmp = template_str;
+	template_str = do_expand_template(template_str);
+	free(tmp);
 	ft_printf("ext: parsed_str >%s< template >%s<\n", parsed_str, template_str);
-	if (res == sucsses)
-		res = do_update_res(args, parsed_str, &template_str);
+	if (parsed_str != NULL && template_str != NULL)
+		ret = do_update_res(args, parsed_str, template_str);
 	free(parsed_str);
 	free(template_str);
-	return (res);
+	return (ret);
 }
 
 static inline void	do_exit(char *cmd, t_extract *args, char **env)
