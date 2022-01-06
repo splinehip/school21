b6296695ed4cf3407a2802334b436ddf588a76c1
Kira <rarakira@gmail.com>
2022-01-06 14:35:01 +0300

    minishell: update builtin commands name check

--- minishell/srcs/actions_handler/do_actions_build.c
+++ minishell/srcs/actions_handler/do_actions_build.c
@@ -6,7 +6,7 @@
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:47:08 by cflorind          #+#    #+#             */
-/*   Updated: 2022/01/06 14:21:25 by lbaela           ###   ########.fr       */
+/*   Updated: 2022/01/06 14:30:41 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,19 @@ static inline void	set_action_type(t_action *action)
 	if (action->exec.argv == NULL || !action->exec.argv[0])
 		return ;
 	name = action->exec.argv[0];
-	if (ft_strncmp("echo", name, ft_strlen("echo")) == 0)
+	if (ft_strncmp("echo", name, ft_strlen("echo") + 1) == 0)
 		action->type = echo;
-	else if (ft_strncmp("cd", name, ft_strlen("cd")) == 0)
+	else if (ft_strncmp("cd", name, ft_strlen("cd") + 1) == 0)
 		action->type = cd;
-	else if (ft_strncmp("pwd", name, ft_strlen("pwd")) == 0)
+	else if (ft_strncmp("pwd", name, ft_strlen("pwd") + 1) == 0)
 		action->type = pwd;
-	else if (ft_strncmp("export", name, ft_strlen("export")) == 0)
+	else if (ft_strncmp("export", name, ft_strlen("export") + 1) == 0)
 		action->type = export;
-	else if (ft_strncmp("unset", name, ft_strlen("unset")) == 0)
+	else if (ft_strncmp("unset", name, ft_strlen("unset") + 1) == 0)
 		action->type = unset;
-	else if (ft_strncmp("env", name, ft_strlen("env")) == 0)
+	else if (ft_strncmp("env", name, ft_strlen("env") + 1) == 0)
 		action->type = env;
-	else if (ft_strncmp("exit", name, ft_strlen("exit")) == 0)
+	else if (ft_strncmp("exit", name, ft_strlen("exit") + 1) == 0)
 		action->type = exit_built;
 	else
 		action->type = execute;
