9c2477c2426dbc66c4ed362608d26e9cfec93407
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2022-01-26 18:18:21 +0300

    minirt: fix define dummy mlx_loop_end for mac os

--- minirt/headers/minirt.h
+++ minirt/headers/minirt.h
@@ -6,12 +6,16 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 11:49:59 by cflorind          #+#    #+#             */
-/*   Updated: 2022/01/26 18:11:21 by cflorind         ###   ########.fr       */
+/*   Updated: 2022/01/26 18:17:56 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
+#
+#include "args.h"
+#
+# define PROG_NAME "miniRT Project"
 # ifdef __APPLE__
 #  define MACOS 1
 
@@ -20,8 +24,6 @@ int	mlx_loop_end(void *mlx);
 # else
 #  define MACOS 0
 # endif
-
-# define PROG_NAME "miniRT Project"
 
 int		main(int argc, char **argv);
 void	free_args(t_args *args);
