349a2491bb5d121622211c21a7d80618c3986c58
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2022-01-26 14:17:37 +0300

    minirt: fix get_scree_size.m for macos

--- minirt/headers/get_screen_size.h
+++ minirt/headers/get_screen_size.h
@@ -6,7 +6,7 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 13:09:03 by cflorind          #+#    #+#             */
-/*   Updated: 2022/01/26 13:49:17 by cflorind         ###   ########.fr       */
+/*   Updated: 2022/01/26 14:15:40 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 # define GET_SCREEN_SIZE_H
 # ifdef __APPLE__
 #  define MACOS 1
-#  int	macos_get_screen_size(int *size_x, int *size_y);
+
+int	macos_get_screen_size(int *size_x, int *size_y);
+int	mlx_get_screen_size(void *mlx, int *x, int *y);
 # else
 #  define MACOS 0
-#  include "mlx.h"
 # endif
 
 int	get_screen_size(void *mlx, int *x, int *y);
--- minirt/srcs/macos_get_screen_size.m
+++ minirt/srcs/macos_get_screen_size.m
@@ -6,11 +6,16 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 13:45:38 by cflorind          #+#    #+#             */
-/*   Updated: 2022/01/26 13:47:57 by cflorind         ###   ########.fr       */
+/*   Updated: 2022/01/26 14:16:04 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #import <AppKit/NSOpenGLView.h>
+
+int	mlx_get_screen_size(void *mlx, int *x, int *y)
+{
+	return (0);
+}
 
 int	macos_get_screen_size(int *size_x, int *size_y)
 {
