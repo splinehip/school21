af6f921f211c72fb072b313a6b71f67c9d649260
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2022-05-11 23:54:04 +0300

    webserv: update Cfg struct

--- webserv/headers/Server.h
+++ webserv/headers/Server.h
@@ -6,7 +6,7 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 17:25:38 by cflorind          #+#    #+#             */
-/*   Updated: 2022/05/05 16:44:14 by cflorind         ###   ########.fr       */
+/*   Updated: 2022/05/11 23:25:23 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include <string>
 #include <map>
+#include <set>
 #include <arpa/inet.h>
 
 #include "logger/Log.h"
@@ -22,12 +23,13 @@
 
 class Server
 {
-typedef std::map<std::string, std::string>  t_mime;
-
+struct Cfg;
 typedef std::map<std::string, std::string>  t_cfg;
-typedef std::map<std::string, t_cfg>        t_name_srvs;
-typedef std::map<int, t_name_srvs>          t_listen_srvs;
-typedef std::map<in_addr_t, t_listen_srvs>  t_srvs;
+typedef std::map<std::string, t_cfg *>      t_name_srvs;
+typedef std::map<int, struct Cfg>           t_listen_ports;
+typedef std::map<in_addr_t, t_listen_ports> t_srvs;
+
+typedef std::map<std::string, std::string>  t_mime;
 
 private:
     std::string cfg_file;
@@ -42,6 +44,15 @@ public:
     ~Server(void);
 };
 
+struct Server::Cfg
+{
+    t_name_srvs name_srvs;
+
+    ~Cfg(void);
+    t_cfg   &operator[](const std::string &key);
+    t_cfg   &addAlterName(const std::string &name_server,
+                const std::string &alter_name_server);
+};
+
 in_addr_t    aton(std::string cp);
 char        *ntoa(in_addr_t in_addr);
-
--- webserv/srcs/Server.cpp
+++ webserv/srcs/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: cflorind <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 17:25:12 by cflorind          #+#    #+#             */
-/*   Updated: 2022/05/05 16:37:45 by cflorind         ###   ########.fr       */
+/*   Updated: 2022/05/11 23:52:12 by cflorind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,70 @@ Server::Server(char **argv)
     this->parseCfg();
 }
 
-Server::~Server(){};
+Server::~Server(void){};
+
+Server::Cfg::~Cfg(void)
+{
+    std::set<t_cfg *>       deleted;
+    t_name_srvs::iterator   it;
+
+    for (it = this->name_srvs.begin(); it != this->name_srvs.end(); ++it)
+    {
+        if (deleted.find(it->second) == deleted.end())
+        {
+            deleted.insert(it->second);
+            delete it->second;
+        }
+    }
+}
+
+Server::t_cfg   &Server::Cfg::operator[](const std::string &key)
+{
+    t_name_srvs::iterator it;
+
+    it = this->name_srvs.find(key);
+    if (it == this->name_srvs.end())
+    {
+        this->name_srvs[key] = new t_cfg;
+    }
+    return (*this->name_srvs[key]);
+}
+
+Server::t_cfg   &Server::Cfg::addAlterName(
+    const std::string &name_server, const std::string &alter_name_server)
+{
+    t_name_srvs::iterator it;
+
+    it = this->name_srvs.find(name_server);
+    if (it == this->name_srvs.end())
+    {
+        this->name_srvs[name_server] = new t_cfg;
+    }
+    this->name_srvs[alter_name_server] = this->name_srvs[name_server];
+    return (*this->name_srvs[alter_name_server]);
+}
 
 void    Server::parseCfg(void)
 {
     logger::Log &log = logger::Log::getInst();
-    log(logger::INFO, "Using configuration file: %s",
-        this->cfg_file.c_str());
+    log(logger::INFO, "Using configuration file: %s", this->cfg_file.c_str());
+    this->srvs[aton("127.0.0.1")][80].addAlterName("localhost", "alterhost");
     this->srvs
-        [aton("127.0.0.1")][80]["localhost"]["listen_addr"] = "127.0.0.1";
-    this->srvs
-        [aton("127.0.0.1")][80]["testhost"]["listen_addr"] = "127.0.0.1";
-    std::cout << "listen_addr: "
+        [aton("127.0.0.1")][80]["alterhost"]["listen_addr"] = "127.0.0.1";
+    this->srvs[aton("127.0.0.1")][80]["alterhost"]["server_name"] = "localhost";
+    this->srvs[aton("127.0.0.1")][80]["testhost"]["listen_addr"] = "127.0.0.2";
+    std::cout
+        << "localhost:listen_addr: "
         << this->srvs[aton("127.0.0.1")][80]["localhost"]["listen_addr"]
+        << "\nlocalhost:server_name: "
+        << this->srvs[aton("127.0.0.1")][80]["localhost"]["server_name"]
+        << std::endl;
+    std::cout << "testhost:listen_addr: "
+        << this->srvs[aton("127.0.0.1")][80]["testhost"]["listen_addr"]
         << std::endl;
     std::cout << ntoa(this->srvs.find(aton("127.0.0.1"))->first) << std::endl;
-    t_srvs::iterator lsrvs = this->srvs.find(aton("127.0.0.1"));
-    if (lsrvs != this->srvs.end())
-        std::cout << ntoa(lsrvs->first) << std::endl;
+    t_srvs::iterator srvs = this->srvs.find(aton("127.0.0.1"));
+    if (srvs != this->srvs.end())
+        std::cout << ntoa(srvs->first) << std::endl;
+
 }
