ffa333b00e8454ac36a8475e2bcd95c88a55560b
Oleg Fedorchenko <o.fedorchenko@unitarm.ru>
2022-07-25 18:03:38 +0300

    ft_w: update Config

--- headers/utils.hpp
+++ headers/utils.hpp
@@ -4,6 +4,24 @@
 
 namespace utl
 {
+static const int POST = 0x01;
+static const int GET = 0x02;
+static const int PUT = 0x04;
+static const int DELETE = 0x08;
+
+unsigned short  httpMethod(const std::string &str)
+{
+    if (str == "POST")
+        return POST;
+    if (str == "GET")
+        return GET;
+    if (str == "PUT")
+        return PUT;
+    if (str == "DELETE")
+        return DELETE;
+    return 0;
+}
+
 int     readFileToString(int fd, std::string &str)
 {
     char buf[4096];
@@ -15,6 +33,20 @@ int     readFileToString(int fd, std::string &str)
         res = read(fd, &buf, 4096);
     }
     return res;
+}
+
+int setNonBlock(int &fd)
+{
+    int flags;
+
+    #if defined (O_NONBLOCK)
+        if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
+            flags = 0;
+        return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    #else
+        flags = 1;
+        return ioctl(fd, FIOBIO, &flags);
+    #endif
 }
 
 }
